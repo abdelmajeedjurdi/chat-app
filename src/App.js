d935c8015dfea46202e9b057e8c0901d12295fca
abdelmajeedjurdi <abdelmajeedjurdi6@gmail.com>
2020-04-02 22:00:53 +0300

    chat-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+ class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '',
+    isConnected:false,
+    id:null,
+    list:[],
+    name:"Abd ELmajeed",
+    message:""};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    console.log(this.state.value)
+  }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }    
+socket = null
+  componentWillMount(){  this.socket = io('https://codi-server.herokuapp.com');
+
+ this.socket.on('pong!',(additionalStuff)=>{
+    console.log('server answered!', additionalStuff)
+    })
+
+this.socket.on('youare',(answer)=>{
+  this.setState({id:answer.id})
+  })
+  this.socket.on('connect', () => {
+this.setState({isConnected:true})
+})
+
+   this.socket.on('disconnect', () => {
+this.setState({isConnected:false})
+})
+
+this.socket.on('pong!',()=>{
+  console.log('the server answered!');
+  })
+
+ 
+    this.socket.on('peeps',(id)=>{
+      const map1 = id.map(x => x + 1);
+      this.setState({list:map1})
+     
+    })
+    this.socket.on('next',(message_from_server)=>console.log(message_from_server))
+
+    this.socket.on('answer',(event)=>{
+    this.setState({text: event.target.value});
+    })  /** this will be useful way, way later **/
+this.socket.on('room', old_messages => console.log(old_messages))  }
+ componentWillUnmount(){
+this.socket.close()
+this.socket = null
 }
-
-export default App;
+ render() {
+return (
+<div className="App">
+<div>status: {this.state.isConnected ? 'connected' : 'disconnected'}</div>
+<div>id: {this.state.id}</div>
+<div><ol>{this.state.list.map(reptile => <li>{reptile}</li>)}</ol> </div>
+<div><input type="text" onChange={this.handleChange}/></div>
+<button onClick={()=>this.socket.emit('ping!')}>ping</button>
+<button onClick={()=>this.socket.emit('whoami')}>Who am I?</button>
+<button onClick={()=>this.socket.emit('give me next')}>messages</button>
+<button onClick={()=>this.socket.emit('addition')}>calculate</button>
+<button onClick={() =>this.socket.send({id: this.state.id,name: "Abd ELmajeed",text: this.state.value})}>submit</button>
+</div>
+);}
+}
+ export default App;
